7d0a79f1f156de5742b4ef40ea14a8cf2026f995
Christos Vasileiou <ChrisVasileiou06@gmail.com>
2025-03-15 15:01:16 +0200

    initial commit

---
+++ Peanut/Src/Core/Defines.h
@@ -0,0 +1,28 @@
+/* 
+ * Copyright (C) 2025 Christos Vasileiou
+ * 
+ * This file is part of Peanut.
+ *
+ * peanut is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Peanut is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Peanut.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#ifndef PEANUT_CORE_DEFINES_H_
+#define PEANUT_CORE_DEFINES_H_
+
+#include "Pch/Pch.h"
+
+#define PEANUT_NO_ERROR uint16_t(0)
+
+#endif // PEANUT_CORE_DEFINES_H_
