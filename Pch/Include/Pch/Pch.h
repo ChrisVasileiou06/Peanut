d520726d8b62c458b8de07c47ef53426643f7701
Christos Vasileiou <ChrisVasileiou06@gmail.com>
2025-03-16 17:43:48 +0200

    fixed copyright notice

--- Pch/Include/Pch/Pch.h
+++ Pch/Include/Pch/Pch.h
@@ -1,20 +1,20 @@
 /* 
  * Copyright (C) 2025 Christos Vasileiou
  * 
- * This file is part of peanut.
+ * This file is part of Peanut.
  *
- * peanut is free software: you can redistribute it and/or modify
+ * Peanut is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * peanut is distributed in the hope that it will be useful,
+ * Peanut is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with peanut.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Peanut.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 
